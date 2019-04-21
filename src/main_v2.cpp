cd4d3d3040e0fe2d80a502e5c0c599064f72dd8b
soyaoki <soya0804@gmail.com>
2019-04-21 13:00:06 +0000

    recommit

--- src/main_v2.cpp
+++ src/main_v2.cpp
@@ -7,8 +7,6 @@
 #include "json.hpp"
 #include "spline.h"
 
-using namespace std;
-
 // for convenience
 using nlohmann::json;
 using std::string;
@@ -16,6 +14,13 @@ using std::vector;
 
 double ref_vel = 0.0; // target velocity
 int lane = 1; // left : 0, middle : 1, right : 2;
+double s_pre1 = 124.834;
+double s_pre2 = 124.834;
+double s_pre3 = 124.834;
+double d_pre1 = 6.16483;
+double d_pre2 = 6.16483;
+double d_pre3 = 6.16483;
+int count = 0;
 
 int main() {
   uWS::Hub h;
@@ -53,11 +58,9 @@ int main() {
     map_waypoints_dx.push_back(d_x);
     map_waypoints_dy.push_back(d_y);
   }
-  ofstream log_file;
-  log_file.open("path_planning_log.csv");
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy,&log_file]
+               &map_waypoints_dx,&map_waypoints_dy]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -103,9 +106,6 @@ int main() {
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
-          std::ofstream single_iteration_log;
-          single_iteration_log.open("path_planning_log-single_iteration.csv");
-          
           std::cout << "--------------------------------------" << std::endl;
           std::cout << "car_x : " << car_x << std::endl;
           std::cout << "car_y : " << car_y << std::endl;
@@ -113,72 +113,31 @@ int main() {
           std::cout << "car_s : " << car_s << std::endl;
           std::cout << "car_d : " << car_d << std::endl;
           std::cout << "car_speed  : " << car_speed  << std::endl;
+          std::cout << "json  : " << j[1]  << std::endl;
+          count++;
+          std::cout << "count  : " << count  << std::endl;
           
-          double desired_d = 2+4*lane; // target d depended lane number
           bool too_close = false; // acc or deacc flag
           int prev_size = previous_path_x.size(); // number of previous path existing
           std::cout << "prev_size : " << prev_size << std::endl;
           
-          // if previous path exists
-          if (prev_size > 0)
-          {
-            car_s = end_path_s;
-            car_d = end_path_d;
-          }
-          
-          // keep previous path
-//          if(prev_size >20)
-//          {
-            for (int i = 0; i < prev_size; i++)
-            {
-              next_x_vals.push_back(previous_path_x[i]);
-              next_y_vals.push_back(previous_path_y[i]);
-            }
-//          }
-          
           // sensing aroud
-          bool left_lane = true;
-          bool right_lane = true;
           for (int i = 0; i < sensor_fusion.size(); i++)
           {
             float d = sensor_fusion[i][6];
-            std::cout << "d : " << d << std::endl;
-            float s = sensor_fusion[i][5];
-            // left lane sensing
-            if (abs(s - car_s) < 30 &&  d < (2+4*(lane-1)+2) && d > (2+4*(lane-1)-2) )
-            {
-              left_lane = false;
-            }
-            
-            // right lane sensing
-            if (abs(s - car_s) < 30 &&  d < (2+4*(lane+1)+2) && d > (2+4*(lane+1)-2) )
-            {
-              right_lane = false;
-            }
-            
-           // current lane sensing
-            if ( d < (2+4*lane+2) && d > (2+4*lane-2) )
+            if ( d < (2+4*lane+2) && d < (2+4*lane-2) )
             {
               double vx = sensor_fusion[i][3];
               double vy = sensor_fusion[i][4];
               double check_speed = sqrt(vx*vx+vy*vy);
               double check_car_s = sensor_fusion[i][5];
               
-              // ???
               check_car_s += ((double)prev_size*.02*check_speed);
               
-              // other vehicle exists in front of and near ego vehicle 
               if ( (check_car_s > car_s) && ((check_car_s - car_s) < 30 )) 
               {
                 too_close = true;
-                if (left_lane == true && lane >= 1)
-                { 
-                  lane = lane - 1;
-                }
-                else if (right_lane == true && lane <= 1)
-                { 
-                  lane = lane + 1;
-                }
+                lane = 0;
               }
             }
           }
@@ -187,84 +146,110 @@ int main() {
           {
             ref_vel -= .224; //deacc
           }
-          else if(ref_vel < 48) // under target velocity
+          else if(ref_vel < 49.5) // under target velocity
           {
             ref_vel += .224; //acc
           }
-          
+            
           std::cout << "ref_vel: " << ref_vel << std::endl;
           
-          // build time vector
-          vector<double> T;
-          int path_points_num = 50;
-          for ( int ind = 1; ind < path_points_num+1; ind ++)
+          if (count < 0)
           {
-            T.push_back(ind*.01); // ego vehicle moves every 0.02
-          }
-          
-          // calculate polynominal to minimize jark
-          vector<double> start_s{car_s, car_speed/2.237, 0};
-          std::cout << "start s :" << start_s[0] << "," << start_s[1] << "," << start_s[2] << std::endl;
-          
-          vector<double> end_s{car_s+(T[path_points_num-1]*ref_vel/2.237), ref_vel/2.237, 0};
-          std::cout << "end s :" << end_s[0] << "," << end_s[1] << "," << end_s[2] << std::endl;
-          
-          vector<double> start_d{car_d, 0, 0};
-          std::cout << "start d :" << start_d[0] << "," << start_d[1] << "," << start_d[2] << std::endl;
-          
-          double ref_d;
-          if (ref_vel <= 10) // mph
-          {
-            ref_d = car_d;
+            double dist_inc = 0.5;
+            for (int i = 0; i < 50; ++i) {
+              next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
+              next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
+            }
           }else
-          {
-            ref_d = 2+4*lane;
-          }
-          vector<double> end_d{ref_d, 0, 0};
-          std::cout << "end d :" << end_d[0] << "," << end_d[1] << "," << end_d[2] << std::endl;
-          
-          vector<double> s_eq = JMT(start_s, end_s, T[path_points_num-1]);
-          std::cout << "s_eq :" << s_eq[0] <<  "," << s_eq[1] << "," <<  s_eq[2] << "," <<  s_eq[4] << "," <<  s_eq[4] << "," <<  s_eq[5] << std::endl;
-          vector<double> d_eq = JMT(start_d, end_d, T[path_points_num-1]);
-          std::cout << "d_eq :" << d_eq[0] <<  "," <<  d_eq[1] << "," <<  d_eq[2] << "," <<  d_eq[4] << "," <<  d_eq[4] << "," <<  d_eq[5] << std::endl;
-          
-//          if (prev_size < 20)
-//          {
-          if (path_points_num - prev_size > 0)
-          {
-            int t_size = T.size();
-            for (int jj = 0; jj < path_points_num-1; jj++)
-//            for (int jj = 0; jj < path_points_num - prev_size; jj++)
             {
-              double tt = T[jj];
-              std::cout << "T :" << tt << std::endl;            
-              double s = cul_polynomial(s_eq,tt);
-              double d = cul_polynomial(d_eq,tt);
-              std::cout << "s :" << s << ", d :"  << d << std::endl;            
-              vector<double> xy = getXY(s, d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-              next_x_vals.push_back(xy[0]);
-              std::cout << "x added to path: " << xy[0] << std::endl;
-              next_y_vals.push_back(xy[1]);
-              std::cout << "y added to path: " << xy[1] << std::endl;
+
+            // build time vector
+            vector<double> T;
+            int path_points_num = 50;
+            for ( int ind = 1; ind < path_points_num+1; ind ++)
+            {
+              T.push_back(ind*.02); // ego vehicle moves every 0.02
+            }
+
+            double T_end = 1; // 2 seconds if car_d center of line
+            // si si_dot si_ddot: to be retrieved
+            double si = car_s;
+            double si_dot = (car_s - s_pre1)/0.02;
+            double si_ddot = ( ((car_s - s_pre1)/0.02) - ((s_pre1 - s_pre2)/0.02) ) / 0.02;
+            double di = car_d;
+            double di_dot = (car_d - d_pre1)/0.02;
+            double di_ddot = ( ((car_d-d_pre1)/0.02) - ((d_pre1-d_pre2)/0.02) ) / 0.02;
+            double sf, sf_dot, sf_ddot;
+            double df, df_dot, df_ddot;
+            if (ref_vel <= 10) // mph
+            {
+              // special handling at low speed: cf werling paper
+              df = di;
+              df_dot = 0;
+              df_ddot = 0;
+              sf_ddot = 0;
+              sf_dot = ref_vel/2.23694;
+              sf = si + 2 * sf_dot * T_end;
+            }
+            else
+            {
+              df = 2+4*lane;
+              df_dot=0;
+              df_ddot = 0;
+              sf_ddot = 0;
+              sf_dot = ref_vel/2.23694;
+              sf = si + sf_dot * T_end;
+            }
+            vector<double> start_s = { si, si_dot, si_ddot};
+            vector<double> end_s = { sf, sf_dot, 0};
+            vector<double> start_d = { di, di_dot, di_ddot };
+            vector<double> end_d = { df, df_dot, df_ddot};
+
+            // calculate polynominal to minimize jark
+  //          vector<double> start_s{car_s, car_speed, 0};
+            std::cout << "start s :" << start_s[0] << "," << start_s[1] << "," << start_s[2] << std::endl;
+  //          vector<double> end_s{(car_s+T[path_points_num-1]*ref_vel), ref_vel, 0};
+            std::cout << "end s :" << end_s[0] << "," << end_s[1] << "," << end_s[2] << std::endl;
+  //          vector<double> start_d{car_d, 0, 0};
+            std::cout << "start d :" << start_d[0] << "," << start_d[1] << "," << start_d[2] << std::endl;
+  //          vector<double> end_d{desired_d, 0, 0};
+            std::cout << "end d :" << end_d[0] << "," << end_d[1] << "," << end_d[2] << std::endl;
+
+            vector<double> s_eq = JMT(start_s, end_s, T_end);
+            std::cout << "s_eq :" << s_eq[0] <<  "," << s_eq[1] << "," <<  s_eq[2] << "," <<  s_eq[4] << "," <<  s_eq[4] << "," <<  s_eq[5] << std::endl;
+            vector<double> d_eq = JMT(start_d, end_d, T_end);
+            std::cout << "d_eq :" << d_eq[0] <<  "," <<  d_eq[1] << "," <<  d_eq[2] << "," <<  d_eq[4] << "," <<  d_eq[4] << "," <<  d_eq[5] << std::endl;
+
+            if (prev_size < 15){
+
+              int t_size = T.size();
+              for (int jj = 0; jj < path_points_num-1; jj++)
+              {
+                double tt = T[jj];
+                std::cout << "T :" << tt << std::endl;            
+                double s = cul_polynomial(s_eq,tt);
+                double d = cul_polynomial(d_eq,tt);
+                std::cout << "s :" << s << ", d :"  << d << std::endl;
+                vector<double> xy = getXY(s, d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+                next_x_vals.push_back(xy[0]);
+                std::cout << "x added to path: " << xy[0] << std::endl;
+                next_y_vals.push_back(xy[1]);
+                std::cout << "y added to path: " << xy[1] << std::endl;
+              }
+            }
+
+            std::cout << "path of : "  << std::endl;
+            for (int i =0; i < next_x_vals.size(); i++){
+              std::cout << "x : "  << next_x_vals[i] << ", y : "  << next_y_vals[i] << std::endl;
             }
           }
           
-          log_file << "waypoints" << endl << "x, y" << endl;
-          single_iteration_log << "waypoints" << endl << "x, y" << endl;
-
-          std::cout << "path of : "  << std::endl;
-          for (int i =0; i < next_x_vals.size(); i++)
-          {
-            std::cout << "x : "  << next_x_vals[i] << ", y : "  << next_y_vals[i] << std::endl;
-            
-            log_file << next_x_vals[i] << ", " << next_y_vals[i] << endl;            
-            
-            single_iteration_log << next_x_vals[i] << ", " << next_y_vals[i] << endl;
-          }
-          log_file << endl;
-          single_iteration_log.close();
-          
-          // ------------- End -------------
+          s_pre1 = car_s;
+          s_pre2 = s_pre1;
+          s_pre3 = s_pre2;
+          d_pre1 = car_d;
+          d_pre2 = d_pre1;
+          d_pre3 = d_pre2;
           
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
@@ -300,5 +285,4 @@ int main() {
   }
   
   h.run();
-  log_file.close();
 }
